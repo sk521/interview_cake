331c25f8babeb969842f043ca9c7fc630f580f0b
sk521 <samkwon521@gmail.com>
2018-05-02 15:22:25 -0400

    instructions added

---
+++ productArrayButSelf.js
@@ -0,0 +1,21 @@
+/*
+INSTRUCTIONS:
+
+Write a function getProductsOfAllIntsExceptAtIndex() that takes an array of integers and returns an array of the products.
+
+For example, given:
+
+  [1, 7, 3, 4]
+
+your function would return:
+
+  [84, 12, 28, 21]
+
+by calculating:
+
+  [7 * 3 * 4,  1 * 3 * 4,  1 * 7 * 4,  1 * 7 * 3]
+
+
+*/
+
+
