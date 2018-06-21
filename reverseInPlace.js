e90564eb9434cbff927686ca346c3cdc4882183d
sk521 <samkwon521@gmail.com>
2018-06-21 13:10:42 -0700

    additional test

--- reverseInPlace.js
+++ reverseInPlace.js
@@ -17,5 +17,7 @@ function reverseInPlace(arr) {
   return arr;
 }
 
-console.log(reverseInPlace([1,2,3,4]));
+console.log(reverseInPlace([1,2,3]));
 // output: [3,2,1]
+
+console.log(reverseInPlace([1,2,3,4,5,6,7,8]));
