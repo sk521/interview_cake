991b9d6a453d8addb74568b8415fafe23e20bb8d
sk521 <samkwon521@gmail.com>
2018-06-24 22:52:41 -0700

    commented code and upload solution

--- reverseWords.js
+++ reverseWords.js
@@ -2,7 +2,30 @@
 Write a function reverseWords() that takes a message as an array of characters and reverses the order of the words in-place.
 */
 
+function reverseWords(arr) {
+  reverseChar(arr, 0, arr.length - 1);
 
+  let currentWordStartIndex = 0;
+  for (let i = 0; i <= arr.length; i++) {
+    if (i === arr.length || arr[i] === ' ') {
+
+      reverseChar(arr, currentWordStartIndex, i - 1);
+      currentWordStartIndex = i + 1;
+    }
+  }
+}
+
+
+// helper function that reverses all characters in the arr;
+function reverseChar(word, start, end) {
+  while (start < end) {
+    let temp = word[start];
+    word[start] = word[end];
+    word[end] = temp;
+    start++;
+    end--;
+  }
+}
 
 var message = ['c', 'a', 'k', 'e', ' ',
   'p', 'o', 'u', 'n', 'd', ' ',
