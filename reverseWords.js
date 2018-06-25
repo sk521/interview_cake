1fc56ad7eccd69d2c8948252a2b5b90b897c4f4b
sk521 <samkwon521@gmail.com>
2018-06-24 22:51:35 -0700

    finished reversing words in place

---
+++ reverseWords.js
@@ -0,0 +1,14 @@
+/*
+Write a function reverseWords() that takes a message as an array of characters and reverses the order of the words in-place.
+*/
+
+
+
+var message = ['c', 'a', 'k', 'e', ' ',
+  'p', 'o', 'u', 'n', 'd', ' ',
+  's', 't', 'e', 'a', 'l'];
+
+reverseWords(message);
+
+console.log(message.join(''));
+// prints: 'steal pound cake'
